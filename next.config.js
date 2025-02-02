962fd4d3a76a8b3ad6cf436d1e523cdaec0dc3c9
haige69 <zilanwangluo@gmail.com>
2025-02-02 13:11:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true },
-  env: {},
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/chat": { page: "/chat" },
-    };
+  output: 'export', // 开启静态导出模式
+  // 可选：如果使用图片优化等动态功能，可能需要禁用
+  images: {
+    unoptimized: true,
   },
 };
 module.exports = nextConfig;
